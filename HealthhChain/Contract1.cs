0ab8f5bcb973d319da45b57f05cbf5e3db6792d8
MINI-ITX\denis <deneus18@hotmail.com>
2018-03-11 00:17:05 +1100

    update smart contract

--- HealthhChain/Contract1.cs
+++ HealthhChain/Contract1.cs
@@ -1,15 +1,27 @@
 ﻿using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
-using System;
-using System.Numerics;
+using System.Threading;
 
 namespace HealthhChain
 {
-    public class Contract1 : SmartContract
+    public class Healtchain : SmartContract
     {
-        public static void Main()
+
+        public static string Main (string operation, params object[] args)
         {
-            Storage.Put(Storage.CurrentContext, "Hello", "World");
+            byte[] owner = new byte[] {3, 170, 248, 27, 24, 22, 58, 190, 139, 28, 40, 137, 161, 69, 248, 236, 2, 254, 14, 33, 64, 179, 125, 97, 228, 195, 118, 119, 5, 60, 148, 150, 238 };
+            switch (operation)
+            {
+                case "register":
+                case "delegate": 
+                    Storage.Put(Storage.CurrentContext, (byte[]) args[0], (byte[])args[1]);
+                    return "Master have been added";
+                case "stopDelegate":
+                     Storage.Delete(Storage.CurrentContext, (byte[])args[0]);
+                    return "Master have been deleted";
+                default:
+                    return "Method not found";
+            }
         }
     }
 }
